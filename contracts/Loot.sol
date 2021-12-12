eeffdb3c462c273e4a8cd21dd336afe34ff53d72
dmcclung <dylanmcclung@gmail.com>
2021-12-11 20:10:53 -0600

    Loot happening

--- contracts/Loot.sol
+++ contracts/Loot.sol
@@ -2,27 +2,21 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
 contract Loot is ERC721URIStorage {
 
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-    
     constructor() ERC721 ("Loot", "LOOT"){
         console.log("Deploying Loot");
     }
 
-    function makeNFT() public {
-        uint256 tokenId = _tokenIds.current();
+    function mintLoot(uint256 tokenId) public {
+        require(tokenId >= 0 && tokenId < 1000, "Out of range");
 
         _safeMint(msg.sender, tokenId);
-        console.log("NFT %s minted to %s", tokenId,  msg.sender);
+        console.log("Loot %s minted to %s", tokenId,  msg.sender);
 
         _setTokenURI(tokenId, "https://ipfs.io/ipfs/QmWR2CdE2JcSgqhqK8o7jWaHa1F38Pwq832zzJgkS7MdCc");
-
-        _tokenIds.increment();
     }
 
 }
